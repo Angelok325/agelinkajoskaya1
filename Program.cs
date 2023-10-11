633c583470d44a6fbd381731cf6a5671c0db56e6
4442 <4442@DESKTOP-BT2PDD3>
2023-10-11 14:49:01 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Найдите объем цилиндра\n");
+Console.Write("Введите радиус:");
+double r = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите высоту:");
+double h = Convert.ToDouble(Console.ReadLine());
+double PI = 3.14;
+double d = r * r;
+double v = PI * d * h;
+Console.WriteLine($"Ваш ответ v = {v}");
+
